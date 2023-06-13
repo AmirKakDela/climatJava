f1ca6e073c542acfae6c8a43ad06ddb813174f3f
Amir Akhmetzhanov <amir.akhmetzhanov@magenta-technology.com>
2023-06-13 21:17:40 +0400

    login page

--- src/main/java/com/climat/demo/Controller.java
+++ src/main/java/com/climat/demo/Controller.java
@@ -1,14 +1,20 @@
 package com.climat.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField login_login;
+
+    @FXML
+    private TextField login_password;
+
+    @FXML
+    void login(ActionEvent event) {
+
     }
+
 }
