654955b07ab38428b29cd126b5f382e7f7a17acb
Amir Akhmetzhanov <amir.akhmetzhanov@magenta-technology.com>
2023-06-20 02:52:35 +0400

    .

--- src/main/java/com/climat/demo/MainController.java
+++ src/main/java/com/climat/demo/MainController.java
@@ -1,9 +1,12 @@
 package com.climat.demo;
 
 import com.climat.demo.Models.ParamsModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -14,18 +17,36 @@ import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
     ParamsController paramsController = new ParamsController();
 
     @FXML
+    private ListView<TimeOfDay> timeOfDay;
+
+    @FXML
+    private ListView<TimeOfYear> timeOfYear;
+
+    @FXML
+    private ListView<Fullness> fullness;
+
+    @FXML
+    private ListView<SystemPower> systemPower;
+
+    @FXML
     private TextField suggestTempMin;
+
     @FXML
     private TextField suggestTempMax;
+
     @FXML
     private TextField suggestWetMin;
+
     @FXML
     private TextField suggestWetMax;
+
     @FXML
     private TextField suggestAirMin;
+
     @FXML
     private TextField suggestAirMax;
 
@@ -57,6 +78,9 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeLists();
+        setListenersOnLists();
+        setSuggestParamsByListValues();
         try {
             ParamsModel paramsModel = paramsController.getParams(1);
             setTextFields(paramsModel);
@@ -80,5 +104,49 @@ public class MainController implements Initializable {
 
     public void close(ActionEvent actionEvent) {
         date.getScene().getWindow().hide();
+    }
+
+    public void setListenersOnLists() {
+        timeOfYear.getSelectionModel().select(RoomEnvironmentRecommendation.getCurrentSeason());
+        timeOfDay.getSelectionModel().select(RoomEnvironmentRecommendation.getCurrentTimeOfDay());
+        fullness.getSelectionModel().select(Fullness.Низкая);
+        systemPower.getSelectionModel().select(SystemPower.Средняя);
+    }
+
+    public void setSuggestParamsByListValues() {
+        TimeOfDay currentTimeOfDay = timeOfDay.getSelectionModel().getSelectedItem();
+        TimeOfYear currentTimeOfYear = timeOfYear.getSelectionModel().getSelectedItem();
+        SystemPower currentSystemPower = systemPower.getSelectionModel().getSelectedItem();
+        Fullness currentFullness = fullness.getSelectionModel().getSelectedItem();
+
+        double[] recommendedTemperature = RoomEnvironmentRecommendation.getRecommendedTemperature(currentTimeOfDay, currentTimeOfYear);
+        suggestTempMin.setText(String.valueOf(recommendedTemperature[0]));
+        suggestTempMax.setText(String.valueOf(recommendedTemperature[1]));
+
+        double[] recommendedWet = RoomEnvironmentRecommendation.getRecommendedWet(currentTimeOfYear, currentSystemPower);
+        suggestWetMin.setText(String.valueOf(recommendedWet[0]));
+        suggestWetMax.setText(String.valueOf(recommendedWet[1]));
+
+        double[] recommendedAir = RoomEnvironmentRecommendation.getRecommendedAirQuality(currentTimeOfYear, currentFullness);
+        suggestAirMin.setText(String.valueOf(recommendedAir[0]));
+        suggestAirMax.setText(String.valueOf(recommendedAir[1]));
+    }
+
+    public void initializeLists() {
+        // Инициализация списка timeOfDay
+        ObservableList<TimeOfDay> timeOfDayList = FXCollections.observableArrayList(TimeOfDay.values());
+        timeOfDay.setItems(timeOfDayList);
+
+        // Инициализация списка timeOfYear
+        ObservableList<TimeOfYear> timeOfYearList = FXCollections.observableArrayList(TimeOfYear.values());
+        timeOfYear.setItems(timeOfYearList);
+
+        // Инициализация списка fullness
+        ObservableList<Fullness> fullnessList = FXCollections.observableArrayList(Fullness.values());
+        fullness.setItems(fullnessList);
+
+        // Инициализация списка systemPower
+        ObservableList<SystemPower> systemPowerList = FXCollections.observableArrayList(SystemPower.values());
+        systemPower.setItems(systemPowerList);
     }
 }
---
+++ src/main/java/com/climat/demo/RoomEnvironmentRecommendation.java
@@ -0,0 +1,158 @@
+package com.climat.demo;
+
+
+enum TimeOfDay {
+    Утро,
+    День,
+    Вечер,
+    Ночь
+}
+
+enum TimeOfYear {
+    Зима,
+    Весна,
+    Лето,
+    Осень
+}
+
+enum SystemPower {
+    Низкая,
+    Средняя,
+    Высокая
+}
+
+enum Fullness {
+    Низкая,
+    Средняя,
+    Высокая
+}
+
+public class RoomEnvironmentRecommendation {
+    public static double[] getRecommendedTemperature(TimeOfDay currentTimeOfDay, TimeOfYear currentTimeOfYear) {
+        double[] temperatureRange = new double[2];
+
+        if (currentTimeOfYear == TimeOfYear.Зима) {
+            if (currentTimeOfDay == TimeOfDay.Утро) {
+                temperatureRange[0] = 18.0;
+                temperatureRange[1] = 20.0;
+            } else if (currentTimeOfDay == TimeOfDay.День) {
+                temperatureRange[0] = 20.0;
+                temperatureRange[1] = 22.0;
+            } else if (currentTimeOfDay == TimeOfDay.Вечер) {
+                temperatureRange[0] = 19.0;
+                temperatureRange[1] = 21.0;
+            } else { // НОЧЬ
+                temperatureRange[0] = 17.0;
+                temperatureRange[1] = 19.0;
+            }
+        } else { // Остальные времена года
+            if (currentTimeOfDay == TimeOfDay.Утро) {
+                temperatureRange[0] = 21.0;
+                temperatureRange[1] = 23.0;
+            } else if (currentTimeOfDay == TimeOfDay.День) {
+                temperatureRange[0] = 23.0;
+                temperatureRange[1] = 25.0;
+            } else if (currentTimeOfDay == TimeOfDay.Вечер) {
+                temperatureRange[0] = 22.0;
+                temperatureRange[1] = 24.0;
+            } else { // НОЧЬ
+                temperatureRange[0] = 20.0;
+                temperatureRange[1] = 22.0;
+            }
+        }
+
+        return temperatureRange;
+    }
+
+    public static double[] getRecommendedWet(TimeOfYear currentTimeOfYear, SystemPower currentSystemPower) {
+        double[] humidityRange = new double[2];
+
+        if (currentTimeOfYear == TimeOfYear.Лето) {
+            if (currentSystemPower == SystemPower.Высокая) {
+                humidityRange[0] = 50.0;
+                humidityRange[1] = 60.0;
+            } else if (currentSystemPower == SystemPower.Средняя) {
+                humidityRange[0] = 45.0;
+                humidityRange[1] = 55.0;
+            } else { // Низкая
+                humidityRange[0] = 40.0;
+                humidityRange[1] = 50.0;
+            }
+        } else {
+            if (currentSystemPower == SystemPower.Высокая) {
+                humidityRange[0] = 45.0;
+                humidityRange[1] = 55.0;
+            } else if (currentSystemPower == SystemPower.Средняя) {
+                humidityRange[0] = 40.0;
+                humidityRange[1] = 50.0;
+            } else { // Низкая
+                humidityRange[0] = 35.0;
+                humidityRange[1] = 45.0;
+            }
+        }
+
+        return humidityRange;
+    }
+
+    public static double[] getRecommendedAirQuality(TimeOfYear currentTimeOfYear, Fullness currentFullness) {
+        double[] airQualityRange = new double[2];
+
+        if (currentTimeOfYear == TimeOfYear.Осень) {
+            if (currentFullness == Fullness.Высокая) {
+                airQualityRange[0] = 75.0;
+                airQualityRange[1] = 85.0;
+            } else if (currentFullness == Fullness.Средняя) {
+                airQualityRange[0] = 70.0;
+                airQualityRange[1] = 80.0;
+            } else { // Низкая
+                airQualityRange[0] = 65.0;
+                airQualityRange[1] = 75.0;
+            }
+        } else {
+            if (currentFullness == Fullness.Высокая) {
+                airQualityRange[0] = 70.0;
+                airQualityRange[1] = 80.0;
+            } else if (currentFullness == Fullness.Средняя) {
+                airQualityRange[0] = 65.0;
+                airQualityRange[1] = 75.0;
+            } else { // Низкая
+                airQualityRange[0] = 60.0;
+                airQualityRange[1] = 70.0;
+            }
+        }
+
+        return airQualityRange;
+    }
+
+    public static TimeOfDay getCurrentTimeOfDay() {
+        // Логика определения текущего времени суток
+        int currentHour = java.time.LocalTime.now().getHour();
+
+        if (currentHour >= 5 && currentHour < 12) {
+            return TimeOfDay.Утро;
+        } else if (currentHour >= 12 && currentHour < 18) {
+            return TimeOfDay.День;
+        } else if (currentHour >= 18 && currentHour < 22) {
+            return TimeOfDay.Вечер;
+        } else {
+            return TimeOfDay.Ночь;
+        }
+    }
+
+    public static TimeOfYear getCurrentSeason() {
+        // Логика определения текущего времени года
+
+        int currentMonth = java.time.LocalDate.now().getMonthValue();
+
+        if (currentMonth >= 3 && currentMonth <= 5) {
+            return TimeOfYear.Весна;
+        } else if (currentMonth >= 6 && currentMonth <= 8) {
+            return TimeOfYear.Лето;
+        } else if (currentMonth >= 9 && currentMonth <= 11) {
+            return TimeOfYear.Осень;
+        } else {
+            return TimeOfYear.Зима;
+        }
+    }
+
+}
